4c3508a385d0f5ad679a1d695645eda42932c849
Kophi <esselenock888@gmail.com>
2024-10-19 17:57:17 -0400

    lambda_s3 codes

--- variable.tf
+++ variable.tf
@@ -1,7 +1,7 @@
 # variables.tf
 
 variable "region" {
-  description = "The AWS region to deploy resources in"
+  description = "The AWS region to deploy resource"
   type        = string
   default     = "us-east-1"
 }
